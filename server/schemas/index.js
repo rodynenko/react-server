e479d08b0aa300af7db4a8a95d517589588a0f61
Taras Rodynenko <t.rodynenko@gmail.com>
2019-02-10 22:27:58 +0200

    update GraphQL Schema to handle Article request

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,18 +1,42 @@
 const { gql } = require('apollo-server');
+const Article = require('../models/article.model');
+const User = require('../models/user.model');
 
 const typeDefs = gql`
 	type Query {
-		hello: String
+		article(slug: String!): Article
+	}
+
+	type Article {
+		id: ID!
+		title: String
+		text: String
+		image: String
+		createAt: String
+		isPublished: Boolean
+		publicatedAt: String
+		slug: String
+		author: UserD,
+		comments: [String]
+	}
+
+	type UserD {
+		id: ID!
+		name: String
+		email: String
 	}
 `;
 
-const resolver = {
+const resolvers = {
 	Query: {
-		hello: () => 'world'
+		article: (_, { slug }) => Article.findOne({ slug })
+	},
+	Article: {
+		author: _ => User.findOne({ _id: _.author[0] })
 	}
 };
 
 module.exports = {
 	typeDefs,
-	resolver
+	resolvers
 };
